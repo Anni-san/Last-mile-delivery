efef3bc0697a29f0da54cb6a38b757e09489c6cb
Animesh <unknowndevilcoder@gmail.com>
2026-06-30 12:34:54 +0530

    Updated server.jsfunctionality

--- db.js
+++ db.js
@@ -12,4 +12,4 @@ const pool = new Pool({
 module.exports = {
    query: (text, params) => pool.query(text, params),
     getClient: () => pool.connect(), // NEW: Needed for safe transactions
-};
+};  
