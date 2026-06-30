5de1e79236522caec35154af76fb364dae6c3228
Animesh <unknowndevilcoder@gmail.com>
2026-06-30 12:36:02 +0530

    Added login functionality

--- server.js
+++ server.js
@@ -122,6 +122,7 @@ app.post('/api/orders', async (req, res) => {
     }
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
