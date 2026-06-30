720532e5b7bd8e0ccb5c474d117289b5ac17b625
Animesh <unknowndevilcoder@gmail.com>
2026-06-30 12:35:24 +0530

    Added login functionality

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.post('/api/rates/calculate', async (req, res) => {
 });
 
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
